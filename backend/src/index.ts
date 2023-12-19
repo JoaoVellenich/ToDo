93edfacad3cb9739d6cb49bbb003ffd37e76d031
JoaoVellenich <jpvellenich@gmail.com>
2023-12-19 11:19:12 -0300

    Start project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen("8080", () => {
-  console.log("App is running on http://localhost:8080");
+  console.log("App is running on http://localhost:8080/");
 });
