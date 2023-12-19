72c01317e20c90619fe55e7d9eaf44ce407619b4
JoaoVellenich <jpvellenich@gmail.com>
2023-12-19 18:03:53 -0300

    feat: adding logs to folder

--- backend/src/services/logging/winston.ts
+++ backend/src/services/logging/winston.ts
@@ -3,5 +3,10 @@ import winston from "winston";
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: "../logs/Backend.log",
+    }),
+  ],
 });
