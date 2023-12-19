bd438d3a7c6231e382b7f504f32e0dd8d2c43e23
JoaoVellenich <jpvellenich@gmail.com>
2023-12-19 11:40:38 -0300

    feat: create UserModel

---
+++ backend/src/models/UserModel.ts
@@ -0,0 +1,34 @@
+import * as Sequelize from "sequelize";
+import db from "../config/db";
+
+const UserModel = db.define("User", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+});
+
+export default UserModel;
